a3f386e485a03023b1a4d2f053fc9ebb8870bd14
Leanna Bucio <79386944+oreos-and-ramen@users.noreply.github.com>
2025-12-03 19:09:09 -0700

    Create get_data.py
    Created get data file and added pseudo code file

---
+++ get_data.py
@@ -0,0 +1,9 @@
+# imports: requests
+
+# function: get_data
+# input: latitude, longitude
+# output: python dict
+# ===================
+# get the response from the API by calling the URL with proper params
+# parse the response into python dict
+# return the python dict
