7dbd95d8a225d76af16711c7340a80e4a00cdc0e
Leanna Bucio <79386944+oreos-and-ramen@users.noreply.github.com>
2025-12-03 19:08:19 -0700

    Create clean_data.py
    Created clean data file and added pseudocode plan

---
+++ clean_data.py
@@ -0,0 +1,13 @@
+# i dont want any of the internal data such as generationtime so I will clear all of that
+# for simplicities sake, I look through the dict and save things I want, and ignore the things I don't
+
+# function: clean_data
+# input: list of information to keep, API data (uncleaned) dict
+# output: python dict
+# ==================================
+# create new (empty) dict
+# iterate through the uncleaned dict
+#   if the key matches something on the keep list, add it to the new dict
+#   ignore if it doesn't
+
+# return the new dict
