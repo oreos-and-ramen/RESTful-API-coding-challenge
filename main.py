9c11e10052fd09541eea39c74ef5c31c877f388e
Leanna Bucio <79386944+oreos-and-ramen@users.noreply.github.com>
2025-12-03 20:53:57 -0700

    Update main.py
    Implemented fix_times function in main

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import json
 
 from get_data import get_data
 from clean_data import clean_data
+from change_data import fix_times
 
 def main():
-  raw_data = get_data() # will later add sys as arguments
-  cleaned_data = clean_data(raw_data) # second arg can be sys later
+    raw_data = get_data() # will later add sys as arguments
+    cleaned_data = clean_data(raw_data) # second arg can be sys later
+    fix_times(cleaned_data)
+  
+    return json.dumps(cleaned_data, indent=1)
+    # finished product, ready for use :)
 
-  # call function to change data => this will just change the dict
-
-  return json.dumps(cleaned_data)
-  # finished product, ready for use :)
-
-main()
+print(main())
