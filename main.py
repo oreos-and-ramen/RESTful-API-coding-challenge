97c7f94c0c6dc8d6b9631ac22d0185d84d532637
Leanna Bucio <79386944+oreos-and-ramen@users.noreply.github.com>
2025-12-03 20:11:33 -0700

    Update main.py
    Changed variable from functions name to avoid errors

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ from clean_data import clean_data
 
 def main():
   raw_data = get_data() # will later add sys as arguments
-  clean_data = clean_data(raw_data) # second arg can be sys later
+  cleaned_data = clean_data(raw_data) # second arg can be sys later
 
   # call function to change data => this will just change the dict
 
-  return json.dumps(clean_data)
+  return json.dumps(cleaned_data)
   # finished product, ready for use :)
 
 main()
