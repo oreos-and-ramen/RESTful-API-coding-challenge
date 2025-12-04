6b5561dd22da933e34c9a94d6e4b20de23ef7eed
Leanna Bucio <79386944+oreos-and-ramen@users.noreply.github.com>
2025-12-03 20:55:14 -0700

    Update get_data.py
    fixed indentation

--- get_data.py
+++ get_data.py
@@ -4,13 +4,14 @@ url = "https://api.open-meteo.com/v1/forecast"
 
 # defaults are for Salt Lake City
 def get_data(latitude=40.746216, longitude=-111.905396):
-  params = {
-    "latitude" : latitude,
-    "longitude": longitude,
-    "daily"    : "sunrise,sunset",
-    "timezone" : "auto"
+    params = {
+        "latitude" : latitude,
+        "longitude": longitude,
+        "daily"    : "sunrise,sunset",
+        "timezone" : "auto"
     }
-  response = requests.get(url, params=params)
-  data = response.json()
+    
+    response = requests.get(url, params=params)
+    data = response.json()
 
   return data
