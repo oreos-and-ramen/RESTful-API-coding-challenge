4cedccb3201cf407ee7e78dd29eef6cf65bbf193
Leanna Bucio <79386944+oreos-and-ramen@users.noreply.github.com>
2025-12-03 21:00:29 -0700

    Update change_data.py

--- change_data.py
+++ change_data.py
@@ -1,5 +1,5 @@
 def fix_times(data):
     for var in data["daily"]: #operating under assumtion that this will be based on daily variable
         if var == "sunrise" or "sunset":
-            for i in data["daily"[var]]:
+            for i in data["daily"][var]:
                 i = i.split("T", 1) # ignore past the T and just give the time
