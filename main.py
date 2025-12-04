59c8325d1a42d6b3b48fa4eb92b5dd97d07b5fa3
Leanna Bucio <79386944+oreos-and-ramen@users.noreply.github.com>
2025-12-03 19:09:41 -0700

    Create main.py
    Created main file and added pseduocode plan

---
+++ main.py
@@ -0,0 +1,15 @@
+# imports: json
+
+# hardcoded variables for the functions
+
+# call function to get data from API => create python dictionary object (lets call this raw_data)
+#   (this will need the lat and long of the area, I will hardcode this to Salt Lake City)
+# call function to clean data => create python dict (lets call this clean_data)
+#   (this will need a list of items the user wants to keep, I will hardcode this)
+#   (this will also need the dictionary from the API data)
+# call function to change data => create python dict (lets call this changed_data)
+
+# use the json dumps feature to change the changed_data dict to JSON output
+# save this new JSON data (lets call this json_finished_data)
+
+# print that the object was succesfully created
